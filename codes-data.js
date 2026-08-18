6ad85efcd4ea12e184e35d1b965caf4ba7ac41bf
staticvacant <mewikinn@gmail.com>
2026-08-18 08:06:33 -0500

    Update codes-data.js

--- codes-data.js
+++ codes-data.js
@@ -1,20 +1,21 @@
 // Data sheet where I catalogue current lobby codes.
 const baseCodes = [
-    { code: "born2play", reward: "Unknown", active: true },
-    { code: "maligilume", reward: "Unknown", active: true },
-    { code: "bemorealien", reward: "Unknown", active: true },
-    { code: "dontblockme", reward: "Unknown", active: true },
-    { code: "survivethenight", reward: "Unknown", active: true },
-    { code: "BlockAndRoll", reward: "Unknown", active: true },
-    { code: "GottaGoFast", reward: "Unknown", active: true },
-    { code: "TakeYourHeart", reward: "Unknown", active: true },
-    { code: "PerfectOrder", reward: "Unknown", active: true },
-    { code: "O2Override", reward: "Unknown", active: true },
-    { code: "IWannaFlyHigh", reward: "Unknown", active: true },
-    { code: "MAGILUME", reward: "Unknown", active: true },
-    { code: "Unknown", reward: "Unknown", active: true },
-    { code: "Unknown", reward: "Unknown", active: true },
-    { code: "Unknown", reward: "Unknown", active: true }
+    { code: "BORN2PLAY", reward: "Unknown", source: "Nintendo", link: "https://x.com/NintendoAmerica/status/2089396842798669841", active: true },
+    { code: "8BitBlast", reward: "Unknown", source: "Unknown", link: "", active: true },
+    { code: "maligilume", reward: "Unknown", source: "Unknown", link: "", active: true },
+    { code: "bemorealien", reward: "Unknown", source: "Unknown", link: "", active: true },
+    { code: "dontblockme", reward: "Unknown", source: "Unknown", link: "", active: true },
+    { code: "survivethenight", reward: "Unknown", source: "Unknown", link: "", active: true },
+    { code: "BlockAndRoll", reward: "Unknown", source: "Unknown", link: "", active: true },
+    { code: "GottaGoFast", reward: "Unknown", source: "Unknown", link: "", active: true },
+    { code: "TakeYourHeart", reward: "Unknown", source: "Unknown", link: "", active: true },
+    { code: "PerfectOrder", reward: "Unknown", source: "Unknown", link: "", active: true },
+    { code: "O2Override", reward: "Unknown", source: "Unknown", link: "", active: true },
+    { code: "IWannaFlyHigh", reward: "Unknown", source: "Unknown", link: "", active: true },
+    { code: "MAGILUME", reward: "Unknown", source: "Unknown", link: "", active: true },
+    { code: "Unknown", reward: "Unknown", source: "Unknown", link: "", active: true },
+    { code: "Unknown", reward: "Unknown", source: "Unknown", link: "", active: true },
+    { code: "Unknown", reward: "Unknown", source: "Unknown", link: "", active: true }
 ];
 
 
