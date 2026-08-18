fb89ebafa9e4825ee40047e7a4dea5dcb8ff80af
staticvacant <mewikinn@gmail.com>
2026-08-18 09:03:54 -0500

    Update codes-data.js

--- codes-data.js
+++ codes-data.js
@@ -4,17 +4,17 @@ const baseCodes = [
     { code: "8BitBlast", reward: "Unknown", source: "Xbox", link: "https://x.com/XBOX/status/2089034524046897618", date: "2026-08-16", active: true },
     { code: "BEMOREALIEN", reward: "Unknown", source: "AlienWare", link: "https://x.com/Alienware/status/2089429140709556283", date: "2026-08-17", active: true },
     { code: "SurviveTheNight", reward: "Unknown", source: "Grandma's Favourite Studio", link: "https://x.com/grandmasfaves/status/2087162231230443856", date: "2026-08-11", active: true },
-    { code: "DontBlockMe", reward: "Unknown", source: "Tetris", link: "", date: "2026-08-20", active: true },
-    { code: "LetsBlockAndRoll", reward: "Unknown", source: "Tetris", link: "", date: "2026-08-20", active: true },
-    { code: "GottaGoFast", reward: "Unknown", source: "Sonic", link: "", date: "2026-08-20", active: true },
-    { code: "IWannaFlyHigh", reward: "Unknown", source: "Tails", link: "", date: "2026-08-20", active: true },
+    { code: "DontBlockMe", reward: "Unknown", source: "Fortnite Discord", link: "", date: "2026-08-20", active: true },
+    { code: "LetsBlockAndRoll", reward: "Unknown", source: "Fortnite Discord", link: "", date: "2026-08-20", active: true },
+    { code: "GottaGoFast", reward: "Unlimited Stamina + Speed Boost", source: "Fortnite Discord", link: "https://x.com/FireMonkey/status/2086832948829901078", date: "2026-08-10", active: true },
+    { code: "IWannaFlyHigh", reward: "Unknown", source: "Fortnite Discord", link: "", date: "2026-08-20", active: true },
     { code: "TakeYourHeart", reward: "Unknown", source: "ATLUS West", link: "https://x.com/Atlus_West/status/2087932899559412182", date: "2026-08-13", active: true },
-    { code: "PerfectOrder", reward: "Unknown", source: "Geno", link: "", date: "2026-08-20", active: true },
-    { code: "O2Override", reward: "Unknown", source: "O2", link: "", date: "2026-08-20", active: true },
-    { code: "MAGILUME", reward: "Unknown", source: "Fortnite Brazil", link: "2026-08-20", date: "", active: true },
+    { code: "PerfectOrder", reward: "Unknown", source: "Fortnite Discord", link: "https://x.com/FireMonkey/status/2086838594920972718", date: "2026-08-10", active: true },
+    { code: "O2OVERRIDE", reward: "Unknown", source: "O2", link: "https://x.com/FireMonkey/status/2088224016565780563", date: "2026-08-14", active: true },
+    { code: "MAGILUME", reward: "Unknown", source: "Fortnite Brazil", link: "", date: "2026-08-20", active: true },
     
-    { code: "Looper1", reward: "Unknown", source: "Unknown", link: "", date: "2026-08-20", active: true },
-    { code: "fishstick1", reward: "Unknown", source: "Unknown", link: "", date: "2026-08-20", active: true },
+    { code: "Looper1", reward: "Unknown", source: "Fortnite Twitter", link: "", date: "2026-08-20", active: true },
+    { code: "fishstick1", reward: "Unknown", source: "Fortnite Twitter", link: "", date: "2026-08-20", active: true },
     { code: "OverrideXP", reward: "Unknown", source: "Fortnite", link: "https://x.com/Fortnite/status/2088702226705567916", date: "2026-08-15", active: true }
 ];
 
