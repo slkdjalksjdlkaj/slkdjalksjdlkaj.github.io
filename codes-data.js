82e3610f364b164bc1a6932e7468d59b6ec572b7
staticvacant <mewikinn@gmail.com>
2026-08-18 07:18:45 -0500

    Create codes-data.js

---
+++ codes-data.js
@@ -0,0 +1,6 @@
+// Data sheet where I catalogue current lobby codes.
+const baseCodes = [
+    { code: "SPRITE2026", reward: "Exclusive Starter Sprite", source: "Official Launch Stream", active: true },
+    { code: "RUNNERS500", reward: "500 Season XP", source: "Twitter / X", active: true },
+    { code: "OVERRIDE2026", reward: "Cube Spray & Banner", source: "In-Game Event", active: false }
+];
