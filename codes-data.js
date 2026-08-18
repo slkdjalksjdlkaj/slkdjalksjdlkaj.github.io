794f5f1d9874a658daaa2628a4bfadb5fc6b5cf2
staticvacant <mewikinn@gmail.com>
2026-08-18 08:41:44 -0500

    Update codes-data.js

--- codes-data.js
+++ codes-data.js
@@ -1,21 +1,21 @@
 // Data sheet where I catalogue current lobby codes.
 const baseCodes = [
     { code: "BORN2PLAY", reward: "Unknown", source: "Nintendo of America", link: "https://x.com/NintendoAmerica/status/2089396842798669841", date: "2026-08-17", active: true },
-    { code: "8BitBlast", reward: "Unknown", source: "Unknown", link: "", date: "", active: true },
-    { code: "maligilume", reward: "Unknown", source: "Unknown", link: "", date: "", active: true },
-    { code: "bemorealien", reward: "Unknown", source: "Unknown", link: "", date: "", active: true },
-    { code: "dontblockme", reward: "Unknown", source: "Unknown", link: "", date: "", active: true },
-    { code: "survivethenight", reward: "Unknown", source: "Unknown", link: "", date: "", active: true },
-    { code: "BlockAndRoll", reward: "Unknown", source: "Unknown", link: "", date: "", active: true },
-    { code: "GottaGoFast", reward: "Unknown", source: "Unknown", link: "", date: "", active: true },
-    { code: "TakeYourHeart", reward: "Unknown", source: "Unknown", link: "", date: "", active: true },
-    { code: "PerfectOrder", reward: "Unknown", source: "Unknown", link: "", date: "", active: true },
-    { code: "O2Override", reward: "Unknown", source: "Unknown", link: "", date: "", active: true },
-    { code: "IWannaFlyHigh", reward: "Unknown", source: "Unknown", link: "", date: "", active: true },
-    { code: "MAGILUME", reward: "Unknown", source: "Unknown", link: "", date: "", active: true },
-    { code: "Unknown", reward: "Unknown", source: "Unknown", link: "", date: "", active: true },
-    { code: "Unknown", reward: "Unknown", source: "Unknown", link: "", date: "", active: true },
-    { code: "Unknown", reward: "Unknown", source: "Unknown", link: "", date: "", active: true }
+    { code: "8BitBlast", reward: "Unknown", source: "Xbox", link: "https://x.com/XBOX/status/2089034524046897618", date: "", active: true },
+    { code: "BEMOREALIEN", reward: "Unknown", source: "AlienWare", link: "https://x.com/Alienware/status/2089429140709556283", date: "", active: true },
+    { code: "SurviveTheNight", reward: "Unknown", source: "99 Nights in the Forest", link: "", date: "", active: true },
+    { code: "DontBlockMe", reward: "Unknown", source: "Tetris", link: "", date: "", active: true },
+    { code: "LetsBlockAndRoll", reward: "Unknown", source: "Tetris", link: "", date: "", active: true },
+    { code: "GottaGoFast", reward: "Unknown", source: "Sonic", link: "", date: "", active: true },
+    { code: "IWannaFlyHigh", reward: "Unknown", source: "Tails", link: "", date: "", active: true },
+    { code: "TakeYourHeart", reward: "Unknown", source: "Persona 5", link: "", date: "", active: true },
+    { code: "PerfectOrder", reward: "Unknown", source: "Geno", link: "", date: "", active: true },
+    { code: "O2Override", reward: "Unknown", source: "O2", link: "", date: "", active: true },
+    { code: "MAGILUME", reward: "Unknown", source: "Fortnite Brazil", link: "", date: "", active: true },
+    
+    { code: "Looper1", reward: "Unknown", source: "Unknown", link: "", date: "", active: true },
+    { code: "fishstick1", reward: "Unknown", source: "Unknown", link: "", date: "", active: true },
+    { code: "OverrideXP", reward: "Unknown", source: "Unknown", link: "", date: "", active: true }
 ];
 
 
