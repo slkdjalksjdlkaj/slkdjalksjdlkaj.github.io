762aebf41d00dd8132177edd884c9735167b6e86
staticvacant <mewikinn@gmail.com>
2026-08-18 07:49:59 -0500

    Update codes-data.js

--- codes-data.js
+++ codes-data.js
@@ -1,20 +1,20 @@
 // Data sheet where I catalogue current lobby codes.
 const baseCodes = [
-    { code: "born2play", reward: "Unknown", source: "Unknown", active: true },
-    { code: "maligilume", reward: "Unknown", source: "Unknown", active: true },
-    { code: "bemorealien", reward: "Unknown", source: "Unknown", active: true },
-    { code: "dontblockme", reward: "Unknown", source: "Unknown", active: true },
-    { code: "survivethenight", reward: "Unknown", source: "Unknown", active: true },
-    { code: "BlockAndRoll", reward: "Unknown", source: "Unknown", active: true },
-    { code: "GottaGoFast", reward: "Unknown", source: "Unknown", active: true },
-    { code: "TakeYourHeart", reward: "Unknown", source: "Unknown", active: true },
-    { code: "PerfectOrder", reward: "Unknown", source: "Unknown", active: true },
-    { code: "O2Override", reward: "Unknown", source: "Unknown", active: true },
-    { code: "IWannaFlyHigh", reward: "Unknown", source: "Unknown", active: true },
-    { code: "MAGILUME", reward: "Unknown", source: "Unknown", active: true },
-    { code: "Unknown", reward: "Unknown", source: "Unknown", active: true },
-    { code: "Unknown", reward: "Unknown", source: "Unknown", active: true },
-    { code: "Unknown", reward: "Unknown", source: "Unknown", active: true }
+    { code: "born2play", reward: "Unknown", active: true },
+    { code: "maligilume", reward: "Unknown", active: true },
+    { code: "bemorealien", reward: "Unknown", active: true },
+    { code: "dontblockme", reward: "Unknown", active: true },
+    { code: "survivethenight", reward: "Unknown", active: true },
+    { code: "BlockAndRoll", reward: "Unknown", active: true },
+    { code: "GottaGoFast", reward: "Unknown", active: true },
+    { code: "TakeYourHeart", reward: "Unknown", active: true },
+    { code: "PerfectOrder", reward: "Unknown", active: true },
+    { code: "O2Override", reward: "Unknown", active: true },
+    { code: "IWannaFlyHigh", reward: "Unknown", active: true },
+    { code: "MAGILUME", reward: "Unknown", active: true },
+    { code: "Unknown", reward: "Unknown", active: true },
+    { code: "Unknown", reward: "Unknown", active: true },
+    { code: "Unknown", reward: "Unknown", active: true }
 ];
 
 
