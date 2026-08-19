7dd4690d01046de235cc67cf2495cf8b52388edc
staticvacant <mewikinn@gmail.com>
2026-08-19 17:55:48 -0500

    Update codes-data.js

--- codes-data.js
+++ codes-data.js
@@ -12,12 +12,17 @@ const baseCodes = [
     { code: "PerfectOrder", reward: "Unknown", source: "Fortnite Discord", link: "https://x.com/FireMonkey/status/2086838594920972718", active: true },
     { code: "O2Override", reward: "Unknown", source: "O2", link: "https://x.com/FireMonkey/status/2088224016565780563", active: true },
     { code: "Magilume", reward: "Unknown", source: "Fortnite Brazil", link: "https://x.com/Brasil_Fortnite", active: true },
-    { code: "Chispambo", reward: "Unknown", source: "Fortnite Spanish", link: "https://x.com/Fortnite_ES/status/2089389318687297587", active: true },
+    { code: "Chispambo", reward: "Unknown", source: "Fortnite Spain", link: "https://x.com/Fortnite_ES/status/2089389318687297587", active: true },
     { code: "Abgestaubt", reward: "Unknown", source: "Fortnite German", link: "https://x.com/FortniteDE/status/2089677733353926697", active: true },
-
+    { code: "PerlimPinPin", reward: "Unknown", source: "Fortnite France", link: "", active: true },
+    
     { code: "Looper1", reward: "Unknown", source: "Fortnite Twitter", link: "", active: true },
     { code: "fishstick1", reward: "Unknown", source: "Fortnite Twitter", link: "", active: true },
-    { code: "OverrideXP", reward: "Unknown", source: "Fortnite", link: "https://x.com/Fortnite/status/2088702226705567916", active: true }
+    { code: "OverrideXP", reward: "Unknown", source: "Fortnite", link: "https://x.com/Fortnite/status/2088702226705567916", active: true },
+    
+    { code: "ReachYourImpossible", reward: "Unknown", source: "Unknown", link: "", active: true }
+    
+    
 ];
 
 
