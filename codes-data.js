92b81649ffe2e12309ad46014759b3239515f843
staticvacant <mewikinn@gmail.com>
2026-08-20 01:37:29 -0500

    Update codes-data.js

--- codes-data.js
+++ codes-data.js
@@ -1,26 +1,26 @@
 // Data sheet where I catalogue current lobby codes.
 const baseCodes = [
-    { code: "Born2Play", reward: "Unknown", source: "Nintendo of America", link: "https://x.com/NintendoAmerica/status/2089396842798669841", active: true },
-    { code: "8BitBlast", reward: "Unknown", source: "Xbox", link: "https://x.com/XBOX/status/2089034524046897618", active: true },
-    { code: "BeMoreAlien", reward: "Unknown", source: "AlienWare", link: "https://x.com/Alienware/status/2089429140709556283", active: true },
-    { code: "SurviveTheNight", reward: "Unknown", source: "Grandma's Favourite Studio", link: "https://x.com/grandmasfaves/status/2087162231230443856", active: true },
-    { code: "DontBlockMe", reward: "Unknown", source: "Fortnite Discord", link: "", active: true },
-    { code: "LetsBlockAndRoll", reward: "Unknown", source: "Fortnite Discord", link: "", active: true },
-    { code: "GottaGoFast", reward: "Unlimited Stamina + Speed Boost", source: "Fortnite Discord", link: "https://x.com/FireMonkey/status/2086832948829901078", active: true },
-    { code: "IWannaFlyHigh", reward: "Unknown", source: "Fortnite Discord", link: "", date: "2026-08-20", active: true },
-    { code: "TakeYourHeart", reward: "Unknown", source: "ATLUS West", link: "https://x.com/Atlus_West/status/2087932899559412182", active: true },
-    { code: "PerfectOrder", reward: "Unknown", source: "Fortnite Discord", link: "https://x.com/FireMonkey/status/2086838594920972718", active: true },
-    { code: "O2Override", reward: "Unknown", source: "O2", link: "https://x.com/FireMonkey/status/2088224016565780563", active: true },
-    { code: "Magilume", reward: "Unknown", source: "Fortnite Brazil", link: "https://x.com/Brasil_Fortnite", active: true },
-    { code: "Chispambo", reward: "Unknown", source: "Fortnite Spain", link: "https://x.com/Fortnite_ES/status/2089389318687297587", active: true },
-    { code: "Abgestaubt", reward: "Unknown", source: "Fortnite German", link: "https://x.com/FortniteDE/status/2089677733353926697", active: true },
-    { code: "PerlimPinPin", reward: "Unknown", source: "Fortnite France", link: "", active: true },
+    { code: "Born2Play", reward: "Unknown", internalreward: null, source: "Nintendo of America", link: "https://x.com/NintendoAmerica/status/2089396842798669841", active: true },
+    { code: "8BitBlast", reward: "Unknown", internalreward: null, source: "Xbox", link: "https://x.com/XBOX/status/2089034524046897618", active: true },
+    { code: "BeMoreAlien", reward: "Unknown", internalreward: null, source: "AlienWare", link: "https://x.com/Alienware/status/2089429140709556283", active: true },
+    { code: "SurviveTheNight", reward: "Unknown", internalreward: null, source: "Grandma's Favourite Studio", link: "https://x.com/grandmasfaves/status/2087162231230443856", active: true },
+    { code: "DontBlockMe", reward: "Unknown", internalreward: null, source: "Fortnite Discord", link: "", active: true },
+    { code: "LetsBlockAndRoll", reward: "Unknown", internalreward: null, source: "Fortnite Discord", link: "", active: true },
+    { code: "GottaGoFast", reward: "Unlimited Stamina + Speed Boost", internalreward: null, source: "Fortnite Discord", link: "https://x.com/FireMonkey/status/2086832948829901078", active: true },
+    { code: "IWannaFlyHigh", reward: "Unknown", internalreward: null, source: "Fortnite Discord", link: "", date: "2026-08-20", active: true },
+    { code: "TakeYourHeart", reward: "Unknown", internalreward: null, source: "ATLUS West", link: "https://x.com/Atlus_West/status/2087932899559412182", active: true },
+    { code: "PerfectOrder", reward: "Unknown", internalreward: null, source: "Fortnite Discord", link: "https://x.com/FireMonkey/status/2086838594920972718", active: true },
+    { code: "O2Override", reward: "Unknown", internalreward: null, source: "O2", link: "https://x.com/FireMonkey/status/2088224016565780563", active: true },
+    { code: "Magilume", reward: "Unknown", internalreward: null, source: "Fortnite Brazil", link: "https://x.com/Brasil_Fortnite", active: true },
+    { code: "Chispambo", reward: "Unknown", internalreward: null, source: "Fortnite Spain", link: "https://x.com/Fortnite_ES/status/2089389318687297587", active: true },
+    { code: "Abgestaubt", reward: "Unknown", internalreward: null, source: "Fortnite German", link: "https://x.com/FortniteDE/status/2089677733353926697", active: true },
+    { code: "PerlimPinPin", reward: "Unknown", internalreward: null, source: "Fortnite France", link: "", active: true },
     
-    { code: "Looper1", reward: "Unknown", source: "Fortnite Twitter", link: "", active: true },
-    { code: "fishstick1", reward: "Unknown", source: "Fortnite Twitter", link: "", active: true },
-    { code: "OverrideXP", reward: "Unknown", source: "Fortnite", link: "https://x.com/Fortnite/status/2088702226705567916", active: true },
+    { code: "Looper1", reward: "Unknown", internalreward: null, source: "Fortnite Twitter", link: "", active: true },
+    { code: "fishstick1", reward: "Unknown", internalreward: null, source: "Fortnite Twitter", link: "", active: true },
+    { code: "OverrideXP", reward: "Unknown", internalreward: null, source: "Fortnite", link: "https://x.com/Fortnite/status/2088702226705567916", active: true },
     
-    { code: "ReachYourImpossible", reward: "Unknown", source: "Unknown", link: "", active: true }
+    { code: "ReachYourImpossible", reward: "Unknown", internalreward: null, source: "Unknown", link: "", active: true }
     
     
 ];
