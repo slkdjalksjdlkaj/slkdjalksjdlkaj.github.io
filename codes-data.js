5a0f7d5a810ad7edd85877ec5dd656822cc9e68a
staticvacant <mewikinn@gmail.com>
2026-08-20 06:34:35 -0500

    Update codes-data.js

--- codes-data.js
+++ codes-data.js
@@ -39,6 +39,6 @@ const baseCodes = [
     { code: "LetsBlockAndRoll", reward: "Turns you into a Tetrimino.", internalreward: null, category: "cat4", active: true },
     
     // --- cat5
-    { code: "Looper1", reward: "Unknown", internalreward: null, category: "cat5", active: true },
-    { code: "fishstick1", reward: "Unknown", internalreward: null, category: "cat5", active: true }
+    { code: "Looper1", reward: "Unknown", internalreward: null, category: "cat5", active: false },
+    { code: "fishstick1", reward: "Unknown", internalreward: null, category: "cat5", active: false }
 ];
