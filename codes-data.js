ccf19a9480d6109150c138b51f116ec8c9b1b78d
staticvacant <mewikinn@gmail.com>
2026-08-20 02:09:49 -0500

    Update codes-data.js

--- codes-data.js
+++ codes-data.js
@@ -6,7 +6,7 @@ const baseCodes = [
     { code: "SurviveTheNight", reward: "2 Cheat Code Locators", internalreward: null, source: "Grandma's Favourite Studio", link: "https://x.com/grandmasfaves/status/2087162231230443856", active: true },
     { code: "DontBlockMe", reward: "Unknown", internalreward: null, source: "Fortnite Discord", link: "", active: true },
     { code: "LetsBlockAndRoll", reward: "Unknown", internalreward: null, source: "Fortnite Discord", link: "", active: true },
-    { code: "GottaGoFast", reward: "Cheat Master Sonic Sprite", internalreward: null, source: "Fortnite Discord", link: "https://x.com/FireMonkey/status/2086832948829901078", active: true },
+    { code: "GottaGoFast", reward: "Cheat Master Sonic Sprite", internalreward: "sonic_cheat", source: "Fortnite Discord", link: "https://x.com/FireMonkey/status/2086832948829901078", active: true },
     { code: "IWannaFlyHigh", reward: "Unknown", internalreward: null, source: "Fortnite Discord", link: "", date: "2026-08-20", active: true },
     { code: "TakeYourHeart", reward: "Unknown", internalreward: null, source: "ATLUS West", link: "https://x.com/Atlus_West/status/2087932899559412182", active: true },
     { code: "PerfectOrder", reward: "Unknown", internalreward: null, source: "Fortnite Discord", link: "https://x.com/FireMonkey/status/2086838594920972718", active: true },
@@ -18,7 +18,7 @@ const baseCodes = [
     
     { code: "Looper1", reward: "Unknown", internalreward: null, source: "Fortnite Twitter", link: "", active: true },
     { code: "fishstick1", reward: "Unknown", internalreward: null, source: "Fortnite Twitter", link: "", active: true },
-    { code: "OverrideXP", reward: "Unknown", internalreward: null, source: "Fortnite", link: "https://x.com/Fortnite/status/2088702226705567916", active: true },
+    { code: "OverrideXP", reward: "40k XP", internalreward: null, source: "Fortnite", link: "https://x.com/Fortnite/status/2088702226705567916", active: true },
     
     { code: "ReachYourImpossible", reward: "Unknown", internalreward: null, source: "Unknown", link: "", active: true }
     
